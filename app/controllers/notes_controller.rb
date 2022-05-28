d6158caab8132722bf9f37e2b8fa2d8254c39a98
John Paul <johnarpaul@gmail.com>
2022-05-28 17:57:26 +0100

    scaffolded notes

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,70 @@
+class NotesController < ApplicationController
+  before_action :set_note, only: %i[ show edit update destroy ]
+
+  # GET /notes or /notes.json
+  def index
+    @notes = Note.all
+  end
+
+  # GET /notes/1 or /notes/1.json
+  def show
+  end
+
+  # GET /notes/new
+  def new
+    @note = Note.new
+  end
+
+  # GET /notes/1/edit
+  def edit
+  end
+
+  # POST /notes or /notes.json
+  def create
+    @note = Note.new(note_params)
+
+    respond_to do |format|
+      if @note.save
+        format.html { redirect_to note_url(@note), notice: "Note was successfully created." }
+        format.json { render :show, status: :created, location: @note }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @note.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /notes/1 or /notes/1.json
+  def update
+    respond_to do |format|
+      if @note.update(note_params)
+        format.html { redirect_to note_url(@note), notice: "Note was successfully updated." }
+        format.json { render :show, status: :ok, location: @note }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @note.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /notes/1 or /notes/1.json
+  def destroy
+    @note.destroy
+
+    respond_to do |format|
+      format.html { redirect_to notes_url, notice: "Note was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_note
+      @note = Note.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def note_params
+      params.fetch(:note, {})
+    end
+end

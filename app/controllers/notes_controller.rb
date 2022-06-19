a043836fe92117313e0f989ffc8f6bef31600c15
John Paul <johnarpaul@gmail.com>
2022-06-19 17:04:23 +0100

    added mobile delete button

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -68,7 +68,7 @@ class NotesController < ApplicationController
     @tags = @notes.pluck(:tags).flatten.uniq
     respond_to do |format|
       format.js
-      # format.html { redirect_to notes_url, notice: "Note was successfully destroyed." }
+      format.html { redirect_to notes_url, notice: "Note was successfully destroyed." }
       # format.json { head :no_content }
     end
   end
